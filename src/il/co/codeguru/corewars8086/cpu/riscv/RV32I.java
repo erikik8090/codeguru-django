cdab651d9a0994910a87ff255d72417fdddba4ac
Erikik <erik8090@gmail.com>
2018-12-19 11:56:46 +0200

    Fix rebase issues

--- src/il/co/codeguru/corewars8086/cpu/riscv/RV32I.java
+++ src/il/co/codeguru/corewars8086/cpu/riscv/RV32I.java
@@ -5,12 +5,12 @@ import il.co.codeguru.corewars8086.cpu.riscv.instruction_formats.*;
 public final class RV32I {
     private RV32I(){}
 
-    public static InstructionI  instructionI (Opcode op, int rd, int rs1, int imm) { return new InstructionI(op.opcode, rd, op.funct3,rs1,imm | (op.funct7 << 5));}
-    public static InstructionS  instructionS (Opcode op, int rs1, int rs2, int imm) { return new InstructionS(op.opcode,op.funct3, rs1, rs2, imm);}
-    public static InstructionR  instructionR (Opcode op, int rd, int rs1, int rs2) { return new InstructionR(op.opcode, rd, op.funct3, rs1, rs2, op.funct7);}
-    public static InstructionU  instructionU (Opcode op, int rd, int imm) { return new InstructionU(op.opcode, rd, imm);}
-    public static InstructionSB instructionSB(Opcode op, int rs1, int rs2, int imm) { return new InstructionSB(op.opcode,op.funct3,rs1, rs2, imm);}
-    public static InstructionUJ instructionUJ(Opcode op, int rd, int imm) { return new InstructionUJ(op.opcode, rd, imm);}
+    public static InstructionFormatI instructionI (Instruction.InstructionInfo op, int rd, int rs1, int imm) { return new InstructionFormatI(op.getOpcode(), rd, op.getFunct3(),rs1,imm | (op.getFunct7() << 5));}
+    public static InstructionFormatS instructionS (Instruction.InstructionInfo op, int rs1, int rs2, int imm) { return new InstructionFormatS(op.getOpcode(),op.getFunct3(), rs1, rs2, imm);}
+    public static InstructionFormatR instructionR (Instruction.InstructionInfo op, int rd, int rs1, int rs2) { return new InstructionFormatR(op.getOpcode(), rd, op.getFunct3(), rs1, rs2, op.getFunct7());}
+    public static InstructionFormatU instructionU (Instruction.InstructionInfo op, int rd, int imm) { return new InstructionFormatU(op.getOpcode(), rd, imm);}
+    public static InstructionFormatSB instructionSB(Instruction.InstructionInfo op, int rs1, int rs2, int imm) { return new InstructionFormatSB(op.getOpcode(),op.getFunct3(),rs1, rs2, imm);}
+    public static InstructionFormatUJ instructionUJ(Instruction.InstructionInfo op, int rd, int imm) { return new InstructionFormatUJ(op.getOpcode(), rd, imm);}
 
     public final class OpcodeTypes {
         public static final int OP_IMM = 0x13;
@@ -26,84 +26,53 @@ public final class RV32I {
 
     public static final class Opcodes{
         //BRANCH
-        public static Opcode Beq = new Opcode(OpcodeTypes.BRANCH, 0);
-        public static Opcode Bne = new Opcode(OpcodeTypes.BRANCH, 1);
-        public static Opcode Blt = new Opcode(OpcodeTypes.BRANCH, 4);
-        public static Opcode Bge = new Opcode(OpcodeTypes.BRANCH, 5);
-        public static Opcode Bltu= new Opcode(OpcodeTypes.BRANCH, 6);
-        public static Opcode Bgeu= new Opcode(OpcodeTypes.BRANCH, 7);
+        public static Instruction.InstructionInfo Beq = new Instruction.InstructionInfo("Beq", OpcodeTypes.BRANCH, 0);
+        public static Instruction.InstructionInfo Bne = new Instruction.InstructionInfo("Bne",OpcodeTypes.BRANCH, 1);
+        public static Instruction.InstructionInfo Blt = new Instruction.InstructionInfo("Blt",OpcodeTypes.BRANCH, 4);
+        public static Instruction.InstructionInfo Bge = new Instruction.InstructionInfo("Bge",OpcodeTypes.BRANCH, 5);
+        public static Instruction.InstructionInfo Bltu= new Instruction.InstructionInfo("Bltu",OpcodeTypes.BRANCH, 6);
+        public static Instruction.InstructionInfo Bgeu= new Instruction.InstructionInfo("Bgeu",OpcodeTypes.BRANCH, 7);
 
         //REGISTER
-        public static Opcode Add = new Opcode(OpcodeTypes.OP, 0, 0);
-        public static Opcode Sub = new Opcode(OpcodeTypes.OP, 0, 32);
-        public static Opcode Sll = new Opcode(OpcodeTypes.OP, 1);
-        public static Opcode Slt = new Opcode(OpcodeTypes.OP, 2);
-        public static Opcode Sltu= new Opcode(OpcodeTypes.OP, 3);
-        public static Opcode Xor = new Opcode(OpcodeTypes.OP, 4);
-        public static Opcode Srl = new Opcode(OpcodeTypes.OP, 5, 0);
-        public static Opcode Sra = new Opcode(OpcodeTypes.OP, 5, 32);
-        public static Opcode Or  = new Opcode(OpcodeTypes.OP, 6);
-        public static Opcode And = new Opcode(OpcodeTypes.OP, 7);
+        public static Instruction.InstructionInfo Add = new Instruction.InstructionInfo("Add",OpcodeTypes.OP, 0, 0);
+        public static Instruction.InstructionInfo Sub = new Instruction.InstructionInfo("Sub",OpcodeTypes.OP, 0, 32);
+        public static Instruction.InstructionInfo Sll = new Instruction.InstructionInfo("Sll",OpcodeTypes.OP, 1);
+        public static Instruction.InstructionInfo Slt = new Instruction.InstructionInfo("Slt",OpcodeTypes.OP, 2);
+        public static Instruction.InstructionInfo Sltu= new Instruction.InstructionInfo("Sltu",OpcodeTypes.OP, 3);
+        public static Instruction.InstructionInfo Xor = new Instruction.InstructionInfo("Xor",OpcodeTypes.OP, 4);
+        public static Instruction.InstructionInfo Srl = new Instruction.InstructionInfo("Srl",OpcodeTypes.OP, 5, 0);
+        public static Instruction.InstructionInfo Sra = new Instruction.InstructionInfo("Sra",OpcodeTypes.OP, 5, 32);
+        public static Instruction.InstructionInfo Or  = new Instruction.InstructionInfo("Or",OpcodeTypes.OP, 6);
+        public static Instruction.InstructionInfo And = new Instruction.InstructionInfo("And",OpcodeTypes.OP, 7);
 
         //Store
-        public static Opcode Sb = new Opcode(OpcodeTypes.STORE, 0);
-        public static Opcode Sh = new Opcode(OpcodeTypes.STORE, 1);
-        public static Opcode Sw = new Opcode(OpcodeTypes.STORE, 2);
+        public static Instruction.InstructionInfo Sb = new Instruction.InstructionInfo("Sb",OpcodeTypes.STORE, 0);
+        public static Instruction.InstructionInfo Sh = new Instruction.InstructionInfo("Sh",OpcodeTypes.STORE, 1);
+        public static Instruction.InstructionInfo Sw = new Instruction.InstructionInfo("Sw",OpcodeTypes.STORE, 2);
 
         //Immediate
-        public static Opcode Addi = new Opcode(OpcodeTypes.OP_IMM, 0);
-        public static Opcode Slli = new Opcode(OpcodeTypes.OP_IMM, 1);
-        public static Opcode Slti = new Opcode(OpcodeTypes.OP_IMM, 2);
-        public static Opcode Sltiu= new Opcode(OpcodeTypes.OP_IMM, 3);
-        public static Opcode Xori = new Opcode(OpcodeTypes.OP_IMM, 4);
-        public static Opcode Srli = new Opcode(OpcodeTypes.OP_IMM, 5, 0);
-        public static Opcode Srai = new Opcode(OpcodeTypes.OP_IMM, 5, 32);
-        public static Opcode Ori  = new Opcode(OpcodeTypes.OP_IMM, 6);
-        public static Opcode Andi = new Opcode(OpcodeTypes.OP_IMM, 7);
+        public static Instruction.InstructionInfo Addi = new Instruction.InstructionInfo("Addi",OpcodeTypes.OP_IMM, 0);
+        public static Instruction.InstructionInfo Slli = new Instruction.InstructionInfo("Slli",OpcodeTypes.OP_IMM, 1);
+        public static Instruction.InstructionInfo Slti = new Instruction.InstructionInfo("Slti",OpcodeTypes.OP_IMM, 2);
+        public static Instruction.InstructionInfo Sltiu= new Instruction.InstructionInfo("Sltui",OpcodeTypes.OP_IMM, 3);
+        public static Instruction.InstructionInfo Xori = new Instruction.InstructionInfo("Xori",OpcodeTypes.OP_IMM, 4);
+        public static Instruction.InstructionInfo Srli = new Instruction.InstructionInfo("Srli",OpcodeTypes.OP_IMM, 5, 0);
+        public static Instruction.InstructionInfo Srai = new Instruction.InstructionInfo("Srai",OpcodeTypes.OP_IMM, 5, 32);
+        public static Instruction.InstructionInfo Ori  = new Instruction.InstructionInfo("Ori",OpcodeTypes.OP_IMM, 6);
+        public static Instruction.InstructionInfo Andi = new Instruction.InstructionInfo("Andi",OpcodeTypes.OP_IMM, 7);
 
         //Load
-        public static Opcode Lb  = new Opcode(OpcodeTypes.LOAD, 0);
-        public static Opcode Lh  = new Opcode(OpcodeTypes.LOAD, 1);
-        public static Opcode Lw  = new Opcode(OpcodeTypes.LOAD, 2);
-        public static Opcode Lbu = new Opcode(OpcodeTypes.LOAD, 4);
-        public static Opcode Lhu = new Opcode(OpcodeTypes.LOAD, 5);
+        public static Instruction.InstructionInfo Lb  = new Instruction.InstructionInfo("Lb",OpcodeTypes.LOAD, 0);
+        public static Instruction.InstructionInfo Lh  = new Instruction.InstructionInfo("Lh",OpcodeTypes.LOAD, 1);
+        public static Instruction.InstructionInfo Lw  = new Instruction.InstructionInfo("Lw",OpcodeTypes.LOAD, 2);
+        public static Instruction.InstructionInfo Lbu = new Instruction.InstructionInfo("Lbu",OpcodeTypes.LOAD, 4);
+        public static Instruction.InstructionInfo Lhu = new Instruction.InstructionInfo("Lhu",OpcodeTypes.LOAD, 5);
 
         //Other
-        public static Opcode Lui  = new Opcode(OpcodeTypes.LUI);
-        public static Opcode Auipc= new Opcode(OpcodeTypes.AUIPC);
-        public static Opcode Jal  = new Opcode(OpcodeTypes.JAL);
-        public static Opcode Jalr = new Opcode(OpcodeTypes.JALR);
-
+        public static Instruction.InstructionInfo Lui  = new Instruction.InstructionInfo("Lui",OpcodeTypes.LUI);
+        public static Instruction.InstructionInfo Auipc= new Instruction.InstructionInfo("Auipc",OpcodeTypes.AUIPC);
+        public static Instruction.InstructionInfo Jal  = new Instruction.InstructionInfo("Jal",OpcodeTypes.JAL);
+        public static Instruction.InstructionInfo Jalr = new Instruction.InstructionInfo("Jalr",OpcodeTypes.JALR);
     }
 
-    public static class Opcode {
-        Opcode() {}
-
-        private int opcode;
-        private int funct3;
-        private int funct7;
-
-        Opcode(int type)
-        {
-            this(type, 0);
-        }
-
-        Opcode(int type, int funct3) {
-            this(type, funct3, 0);
-        }
-
-        Opcode(int type, int funct3, int funct7){
-            this.opcode = type;
-            this.funct3 = funct3;
-            this.funct7 = funct7;
-        }
-
-        public int getOpcode() {
-            return opcode;
-        }
-
-        public int getFunct3() {
-            return funct3;
-        }
-    }
 }

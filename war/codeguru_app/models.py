954b3ce2d40f6fbd31e638b29b49ad1370460e3b
Erikik <erik8090@gmail.com>
2019-01-20 20:42:35 +0200

    Add skeleton for team model. WIP

--- war/codeguru_app/models.py
+++ war/codeguru_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Team(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #Additional info
+
+@receiver(post_save, sender=User)
+def create_user_team(sender, instance, created, **kwargs):
+    if created:
+        Team.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_team(sender, instance, **kwargs):
+    instance.team.save()
